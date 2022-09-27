e69b954bc812d9c79b03a16f3e98e4a3eb14e300
HanYiting12 <114421209+HanYiting12@users.noreply.github.com>
2022-09-27 13:01:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -63,7 +63,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '老婆0',
+      name: '逸庭_',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o4WZF6JIhxSuXkPe7x46gLpxFXvU',
       // 你想对他发送的模板消息的模板ID
